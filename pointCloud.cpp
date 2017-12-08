309beef10a8523af165e3255079b2501ec120867
Catherine <cat.taylor.taylot@btinternet.com>
2017-12-08 13:57:12 +0000

    Began refactoring code

--- pointCloud.cpp
+++ pointCloud.cpp
@@ -14,8 +14,17 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/common/transforms.h>
 #include <ros/ros.h>
-#include <pcl/conversions.h>
+#include <pcl/registration/icp.h>
 #include <vector>
+
+cv::Mat getSegmentedDepth(std::string depthFile, std::string maskFile){
+    cv::Mat depth, mask, depthMat;
+    depth = cv::imread(depthFile, cv::IMREAD_ANYDEPTH);
+    mask = cv::imread(maskFile, cv::IMREAD_GRAYSCALE);
+    mask.convertTo(mask, CV_16UC1);
+    depthMat = depth.mul(mask/255.0f);
+    return depthMat;
+}
 
 int main(){
 
@@ -35,22 +44,8 @@ int main(){
 
     Calibration calibration(K, L);
 
-    // std::string DepthMap = "0000.yml";
-    // cv::Mat depthMat; //read from depth map
-    // cv::FileStorage fs(DepthMap, cv::FileStorage::READ);
-    // fs["depth"] >> depthMat;
-
-    // std::cout<<"yml type "<<depthMat.type()<<"\n";
-
-
-    cv::Mat depth = cv::imread("apple.png", cv::IMREAD_ANYDEPTH );
-    cv::Mat mask = cv::imread("mask.png", cv::IMREAD_GRAYSCALE );
-    mask.convertTo(mask, CV_16UC1);
-
-
-    cv::Mat depthMat, seg;
-    depthMat = depth.mul(mask/255.0f);
-
+    cv::Mat depthMat = getSegmentedDepth("coffee.png", "mask.png");
+    cv::Mat depthMat2 = getSegmentedDepth("coffee2.png", "mask2.png");
 
     hVec2D uv ;
     hVec3D xyz;
@@ -61,27 +56,31 @@ int main(){
     cloud->is_dense = false;
     cloud->points.resize (cloud->width * cloud->height);
 
-        for(int i = 0; i< depthMat.rows; ++i){
-            for( int j = 0; j< depthMat.cols; ++j){   
-                if(depthMat.at<ushort>(i,j)!=0){
-                    uv<<j,i,1;
-                    xyz = calibration.Unproject(uv);
-                    xyz *= 1.0f/xyz(2)*depthMat.at<ushort>(i,j);
-                    xyz(3) = 1;
-                    (*cloud)[depthMat.rows*j + i].x=xyz(0);
-                    (*cloud)[depthMat.rows*j + i].y=xyz(1);
-                    (*cloud)[depthMat.rows*j + i].z=xyz(2);
-                    (*cloud)[depthMat.rows*j + i].r=255;
-                    (*cloud)[depthMat.rows*j + i].g=255;
-                    (*cloud)[depthMat.rows*j + i].b=255;
-                }
-                
+    for(int i = 0; i< depthMat.rows; ++i){
+        for( int j = 0; j< depthMat.cols; ++j){   
+            if(depthMat.at<ushort>(i,j)!=0){
+                uv<<j,i,1;
+                xyz = calibration.Unproject(uv);
+                xyz *= 1.0f/xyz(2)*depthMat.at<ushort>(i,j);
+                xyz(3) = 1;
+                (*cloud)[depthMat.rows*j + i].x=xyz(0);
+                (*cloud)[depthMat.rows*j + i].y=xyz(1);
+                (*cloud)[depthMat.rows*j + i].z=xyz(2);
+                (*cloud)[depthMat.rows*j + i].r=255;
+                (*cloud)[depthMat.rows*j + i].g=255;
+                (*cloud)[depthMat.rows*j + i].b=255;
             }
         }
-       pcl::visualization::CloudViewer viewer ("View Point Cloud");
-       viewer.showCloud (cloud);
-      
-        while (!viewer.wasStopped ()){}
+    }
+
+
+
+
+
+    pcl::visualization::CloudViewer viewer ("View Point Cloud");
+    viewer.showCloud (cloud);
+
+    while (!viewer.wasStopped ()){}
 
     return 0;
 }
